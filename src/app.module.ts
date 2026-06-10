b8427a7868f44f51445dcb96146c82fa46498f98
Shreyash Mahalle <shreyashmahallle75@gmail.com>
2026-06-10 12:35:48 +0530

    Configured Neon PostgreSQL for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -13,25 +13,26 @@ import { PatientModule } from './patient/patient.module';
 import { User } from './users/user.entity';
 import { DoctorProfile } from './doctor/doctor-profile.entity';
 import { PatientProfile } from './patient/patient-profile.entity';
-
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Postgres@123',
-      database: 'schedula_db',
+    ConfigModule.forRoot({
+  isGlobal: true,
+}),
+TypeOrmModule.forRoot({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: [
+    User,
+    DoctorProfile,
+    PatientProfile,
+  ],
+  synchronize: false,
+}),
 
-      entities: [
-        User,
-        DoctorProfile,
-        PatientProfile,
-      ],
-
-      synchronize: false,
-    }),
 
     AuthModule,
     UsersModule,
