abee7a89c1439b981c4429585521664ad63d44a1
Shreyash Mahalle <shreyashmahallle75@gmail.com>
2026-06-24 14:37:41 +0530

    docs: update README and appointment module for day 15

--- src/appointment/appointment.module.ts
+++ src/appointment/appointment.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppointmentController } from './appointment.controller';
 import { AppointmentService } from './appointment.service';
 import { NotificationModule } from '../notification/notification.module';
+
 @Module({
   imports: [NotificationModule],
-
   controllers: [AppointmentController],
   providers: [AppointmentService],
 })
